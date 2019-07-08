b0a732e127e34f9867672c8f57242887e24e6c90
Sergey Bondar <Bondar.S.Evg@sberbank.ru>
2019-07-09 01:58:22 +0300

    reactize

--- src/App.js
+++ src/App.js
@@ -1,26 +1,71 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const SKILLS = [
+    'В коммуникациях',
+    'В анализе и планировании задач',
+    'В качестве кода и код-ревью',
+    'При исполнении взятых обязательств',
+    'В саморазвитии',
+    '',
+];
+const LEVELS = [
+    'Не могу оценить',
+    'Безынициативно, но делает',
+    'Так, как все коллеги вокруг',
+    'Хорошо и берет на себя ответственность',
+    'Помогает и другим вне своей ответственности',
+    'Крутотеньски, быть лучше не может',
+];
+
+const App = () => {
+    const [currentLevel, setLevel] = useState(LEVELS.map(() => Math.round(Math.random() * LEVELS.length) - 1));
+    console.log(currentLevel);
+
+    return (
+        <div className="wrapper">
+            <h1>Медузка навыков человеков</h1>
+            <p>Как себя проявляет Алексей Титов на ваш взгляд:</p>
+
+            <div className="octo">
+                <div className="octo-body octo-body-1"/>
+
+                {SKILLS.map((skill, skillKey) => (
+                    <>
+                        <input
+                            className={`skills ${!!skill ? 'skills_disabled' : ''}`}
+                            type="text"
+                            name={`skill${skillKey}`}
+                            value={skill || 'Впишите свое...'}
+                            readOnly={!!skill}
+                        />
+                        <div className={`octo-legs octo-leg${skillKey}`}>
+                            {LEVELS.reduceRight((result, level, levelKey) => {
+                                return result.concat([(
+                                    <>
+                                        <input
+                                            type="radio"
+                                            name={`leg${skillKey}`}
+                                            id={`leg${skillKey}-${levelKey}`}
+                                            value={levelKey}
+                                            checked={currentLevel[skillKey] === levelKey ? 'checked' : ''}
+                                            onClick={() => setLevel(currentLevel.map((iterLevelKey, iterSkillKey) => {
+                                                return iterSkillKey === skillKey ? levelKey : iterLevelKey;
+                                            }))}
+                                        />
+                                        <label htmlFor={`leg${skillKey}-${levelKey}`} title={level}/>
+                                    </>
+                                )])
+                            }, [])}
+                            <div className="progessbar"/>
+                        </div>
+                    </>
+                ))}
+
+            </div>
+        </div>
+    );
+};
 
 export default App;
