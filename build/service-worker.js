3bc9a2f88c6deb5a812f651ebdc853d4550a8cc0
Sergey Bondar <Bondar.S.Evg@sberbank.ru>
2019-07-09 17:24:53 +0300

    public path

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.4390e43dc19467b86bba61f7246616ba.js"
+  "https://marapper.github.io/skillopus/build/precache-manifest.1de3813e814fc5a5e0eaf6e060fa123d.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("https://marapper.github.io/skillopus/build/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
