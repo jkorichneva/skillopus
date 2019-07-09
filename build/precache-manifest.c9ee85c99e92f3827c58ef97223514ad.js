a4d0dd8a82fdf86b0665a076cc6e72ece1765819
Sergey Bondar <Bondar.S.Evg@sberbank.ru>
2019-07-09 23:36:14 +0300

    add scripts

---
+++ build/precache-manifest.c9ee85c99e92f3827c58ef97223514ad.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b7dd53568165a821c60d49d3ef106cb5",
+    "url": "https://marapper.github.io/skillopus/build/index.html"
+  },
+  {
+    "revision": "30597044aeaf1d119ab0",
+    "url": "https://marapper.github.io/skillopus/build/static/css/main.cac17544.chunk.css"
+  },
+  {
+    "revision": "bacd03dd803a880f86e1",
+    "url": "https://marapper.github.io/skillopus/build/static/js/2.f2db194a.chunk.js"
+  },
+  {
+    "revision": "30597044aeaf1d119ab0",
+    "url": "https://marapper.github.io/skillopus/build/static/js/main.e92b68f8.chunk.js"
+  },
+  {
+    "revision": "6819d5714b5a40d1dc4e",
+    "url": "https://marapper.github.io/skillopus/build/static/js/runtime~main.423f72e1.js"
+  }
+]);
