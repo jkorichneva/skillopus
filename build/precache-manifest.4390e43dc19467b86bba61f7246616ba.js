1667264f24837b46555be3116b26dbb28a4076ad
Sergey Bondar <Bondar.S.Evg@sberbank.ru>
2019-07-09 17:22:05 +0300

    feat: gh page

---
+++ build/precache-manifest.4390e43dc19467b86bba61f7246616ba.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4600a643a8678df93313f68d21e39121",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0e68c846a802a4e75352",
+    "url": "/static/css/main.cac17544.chunk.css"
+  },
+  {
+    "revision": "bacd03dd803a880f86e1",
+    "url": "/static/js/2.f2db194a.chunk.js"
+  },
+  {
+    "revision": "0e68c846a802a4e75352",
+    "url": "/static/js/main.9bb7cd5a.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
